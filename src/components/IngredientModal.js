7e046c7d46f53217b7ccd2495c28b818b064667e
JammulaVenkatramaiah <venkyjammula@gmail.com>
2025-09-14 17:10:48 +0530

    final commit

--- src/components/IngredientModal.js
+++ src/components/IngredientModal.js
@@ -3,15 +3,15 @@ function IngredientModal({ dish, onClose }) {
   return (
     <div className="ingredient-modal">
       <div className="modal-dialog modal-dialog-centered">
-        <div className="modal-content">
-          <div className="modal-header">
+        <div className="modal-content ingredient-bg">
+          <div className="modal-header d-flex justify-content-between align-items-center">
             <h5 className="modal-title">{dish.name} Ingredients</h5>
-            <button type="button" className="btn-close" onClick={onClose}></button>
+            <button type="button" className="btn-close-custom" aria-label="Close" onClick={onClose}></button>
           </div>
           <div className="modal-body">
             <ul className="list-group">
               {dish.ingredients.map((ing, idx) => (
-                <li key={idx} className="list-group-item">{ing}</li>
+                <li key={idx} className="list-group-item">{ing.name} - {ing.quantity}</li>
               ))}
             </ul>
           </div>
